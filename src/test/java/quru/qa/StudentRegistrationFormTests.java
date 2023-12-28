e30ee4386a829972d4eb0232142f578959309fcb
anatoliy.aleshin <anaitolya@mail.ru>
2023-12-28 09:56:52 +0300

    Внес изменения после замечаний

---
+++ src/test/java/quru/qa/StudentRegistrationFormTests.java
@@ -0,0 +1,73 @@
+package quru.qa;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class StudentRegistrationFormTests {
+    @Test
+    void firstTest() {
+        Configuration.pageLoadStrategy = "eager";
+        open("https://demoqa.com/automation-practice-form");
+
+        //First Name
+        $("#firstName").setValue("Имя");
+
+        //Last Name
+        $("#lastName").setValue("Фамилия");
+
+        //Email
+        $("#userEmail").setValue("firstlastname@gmail.com");
+
+        //Gender
+        $("[for=gender-radio-1]").click();
+
+        //Mobile
+        $("#userNumber").setValue("1234567891");
+
+        //Date of Birth
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").click();
+        $(".react-datepicker__month-select").selectOption("April");
+        $(".react-datepicker__year-select").click();
+        $(".react-datepicker__year-select").selectOption("2004");
+        $(".react-datepicker__day--004").click();
+
+        //Subjects
+        $("#subjectsInput").setValue("Economics").pressEnter();
+
+        //Hobbies
+        $("[for=hobbies-checkbox-1]").click();
+
+    //Picture
+        String homeworkName = "homework.jpg";
+        $("#uploadPicture").uploadFromClasspath(homeworkName);
+
+    //Current Address
+    $("#currentAddress").setValue("Москва");
+
+    //State and City
+    $("#react-select-3-input").setValue("NCR").pressEnter();
+    $("#react-select-4-input]").setValue("Delhi").pressEnter();
+    $("#submit").click();
+
+    //Проверка полей ввода
+    $(".table").shouldHave(text("Имя"));
+    $(".table").shouldHave(text("Фамилия"));
+    $(".table").shouldHave(text("firstlastname@gmail.com"));
+    $(".table").shouldHave(text("1234567891"));
+    $(".table").shouldHave(text("04 April,2004"));
+    $(".table").shouldHave(text("Economics"));
+    $(".table").shouldHave(text("Sports"));
+    $(".table").shouldHave(text("Homework.png"));
+    $(".table").shouldHave(text("Москва"));
+    $(".table").shouldHave(text("NCR Delhi"));
+}
+
+}
